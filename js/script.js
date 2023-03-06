c7b74558a56ea3773dd1fd486056dfff229a2dd3
Vagner Matos <vagnerrogeriomatos@hotmail.com>
2023-03-06 18:07:49 -0300

    iniciando modo dark

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const mode = document.getElementById('mode-icon');
+
+mode.addEventListener('click', () => {
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+        
+        return;
+    }
+
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+
+});
